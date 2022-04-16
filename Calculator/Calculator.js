bd19f1d48072fd1dbfa11224fb8a9e2e53b3ea53
Ramcharan <68723018+Ramcharanyadav@users.noreply.github.com>
2022-04-16 17:34:06 +0530

    Added dark theme

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -1,23 +1,63 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
 
-buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.id == "clear") {
-      display.innerText = "";
-    } else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-    } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    } else {
-      display.innerText += item.id;
+var displayArea = document.getElementById("display");
+
+function isOperator(buttonText) {
+  if (
+    buttonText == "+" ||
+    buttonText == "-" ||
+    buttonText == "*" ||
+    buttonText == "/" ||
+    buttonText == "%"
+  )
+    return true;
+  else return false;
+}
+function buttonClicked(buttonText) {
+  if(displayArea.innerText.length >= 24)
+    return
+
+  if (buttonText === "CLR") {
+    displayArea.innerText = "0"
+    return
+  }
+
+  if (buttonText === "DEL") {
+    displayArea.innerText = displayArea.innerText.slice(0, -1)
+    return
+  }
+
+  if (buttonText === "="){
+    try {
+      displayArea.innerText = eval(displayArea.innerText);
+    } catch (error) {
+      displayArea.innerText = 0;
     }
-  };
-});
+    return
+  }
+
+  if (displayArea.innerText === "0") {
+    if (buttonText == "0" || buttonText == "00")
+      return
+
+    if (buttonText == "." || isOperator(buttonText)) {
+      displayArea.innerText = '0' + buttonText;
+      return
+    }
+
+    displayArea.innerText = buttonText;
+  } else {
+    if (
+      isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
+      isOperator(buttonText)
+    ) {
+      displayArea.innerText =
+        displayArea.innerText.slice(0, -1) + buttonText
+      return
+    }
+
+    displayArea.innerText = displayArea.innerText + buttonText;
+  }
+}
 
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
@@ -28,3 +68,5 @@ themeToggleBtn.onclick = () => {
   themeToggleBtn.classList.toggle("active");
   isDark = !isDark;
 };
+
+
